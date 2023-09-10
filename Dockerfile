a0d8c227910f73ce78d38f25f40adc58e4805940
Albert <opeyemialbert20@gmail.com>
2023-09-10 01:48:00 +0100

    code

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,9 @@
+#FROM openjdk:17
+#ARG JAR_FILE=target/*.jar
+#COPY ${JAR_FILE} app.jar
+#ENTRYPOINT ["java","-jar","/app.jar"]
+
 FROM openjdk:17
-ARG JAR_FILE=target/*.jar
-COPY ${JAR_FILE} app.jar
-ENTRYPOINT ["java","-jar","/app.jar"]
+EXPOSE 8086
+ADD target/wears-fashionblog.jar wears-fashionblog.jar
+ENTRYPOINT ["java","-jar","/wears-fashionblog.jar"]
--- src/main/java/com/example/wearsfashionblog/WearsFashionBlogApplication.java
+++ src/main/java/com/example/wearsfashionblog/WearsFashionBlogApplication.java
@@ -1,13 +1,21 @@
 package com.example.wearsfashionblog;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDateTime;
+import java.util.TimeZone;
+@Slf4j
 @SpringBootApplication
 public class WearsFashionBlogApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WearsFashionBlogApplication.class, args);
+        log.info(
+                "\n\n ============================ APPLICATION LAUNCHED ======================= \n\n");
+        TimeZone.setDefault(TimeZone.getTimeZone("Africa/Lagos"));
+        System.out.println(LocalDateTime.now());
     }
 
 }
